d3494e177d4b13d5331c49176b9c46e5435b58f8
Shaurya Iyer <Iyershaurya@gmail.com>
2024-11-23 19:02:02 -0600

    Combined all csvs
    Combined all csvs into one dataframe

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 import os
 
 
-PATH = "/Users/shauryaiyer/Documents/GitHub/CourseAnalyzer"
+directory = "/Users/shauryaiyer/Documents/GitHub/CourseAnalyzer/Grade_Distributions"
+
+dfs = []
+
+
+for file in os.listdir(directory):
+    if file.endswith(".csv"):
+        file_name = os.path.splitext(file)[0]
+        term, year = file_name.split()
+        df = pd.read_csv(os.path.join(directory, file))
+        df['Term'] = term
+        df['Year'] = year
+        dfs.append(df)
+
+all_df = pd.concat(dfs, ignore_index=True)
+print(all_df)
+
+
+
+
+
+
