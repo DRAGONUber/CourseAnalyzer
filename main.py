84e2d41af1824fdcf843f62339deac4620130b31
Shaurya Iyer <Iyershaurya@gmail.com>
2024-11-23 18:49:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,4 @@ import matplotlib.pyplot as plt
 import os
 
 
-PATH = "/Users/shauryaiyer/Downloads/"
-
-
-
-
-<<<<<<< HEAD
-PATH = "/Users/shauryaiyer/Desktop"
-=======
->>>>>>> df579b1d46747c3b10f49a7023b2d729f7f6b441
+PATH = "/Users/shauryaiyer/Documents/GitHub/CourseAnalyzer"
