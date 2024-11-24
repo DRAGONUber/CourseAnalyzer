b33df5d5fb1e7a2bef38f4d563605ce842303bd8
rgowda21 <rmgdpm@gmail.com>
2024-11-23 18:52:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,4 @@ import matplotlib.pyplot as plt
 import os
 
 
-<<<<<<< HEAD
-PATH = "/Users/shauryaiyer/Downloads/"
-
-
-
-
-
-
-
-
-=======
 PATH = "/Users/shauryaiyer/Documents/GitHub/CourseAnalyzer"
->>>>>>> 84e2d41af1824fdcf843f62339deac4620130b31
